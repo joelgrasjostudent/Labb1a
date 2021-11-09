ff6aa7e0837651ed190e7c4fd937d532ed6804cf
joelgrasjostudent <grasjo@student.chalmers.se>
2021-11-09 16:15:18 +0100

    javadoc comments for volvo saab movable

--- src/Movable.java
+++ src/Movable.java
@@ -3,10 +3,16 @@
  * interface for movable objects
  */
 public interface Movable {
-	// method representing movement
+	/**
+	 * method representing movement
+	 */
 	void move();
-	// method for turning right
+	/**
+	 * method for turning right
+	 */
 	void turnLeft();
-	// method for turning left
+	/**
+	 * method for turning left
+	 */
 	void turnRight();
 }
