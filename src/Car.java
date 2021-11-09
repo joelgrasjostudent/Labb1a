b4fbbf369da15cff46f09d1b31ad390d0d896c91
caeriks0412 <caeriks@student.chalmers.se>
2021-11-09 16:16:40 +0100

    javadoc coverage

--- src/Car.java
+++ src/Car.java
@@ -19,8 +19,13 @@ public abstract class Car implements Movable{
     private static final int SOUTH = 2; //South constant
     private static final int WEST = 3; //West constant
     
-    
-    //Super constructor, creates a car from doors, color, enginepower, model.
+    /**
+     * Math.max(getCurrentSpeed() - speedFactor() * amount,0)
+     * @param nrDoors
+     * @param color
+     * @param power
+     * @param model
+     */
     public Car(int nrDoors, Color color, int power, String model) {
     	this.nrDoors = nrDoors;
     	this.color = color;
@@ -31,50 +36,86 @@ public abstract class Car implements Movable{
     	y = 0;
     	direction = NORTH;
     }
-	//Returns number of doors
+    /**
+     * Returns number of doors
+     * @return
+     */
     public int getNrDoors(){
         return nrDoors;
     }
-    //Returns the engine power
+    /**
+     * Returns the engine power
+     * @return
+     */
     public double getEnginePower(){
         return enginePower;
     }
-    //Returns the current speed
+    /**
+     * Returns the current speed
+     * @return
+     */
     public double getCurrentSpeed(){
         return currentSpeed;
     }
-    //Set a new current speed
+    /**
+     * Set a new current speed, takes an input speed as double
+     * @param speed
+     */
     public void setCurrentSpeed(double speed) {
     	currentSpeed = speed;
     }
-    //Returns the color the car has
+    /**
+     * Returns the color the car has
+     * @return
+     */
     public Color getColor(){
         return color;
     }
-    //Give the car a new fancy color
+    /**
+     * Give the car a new fancy color, takes the parameter color
+     * @param clr
+     */
     public void setColor(Color clr){
 	    color = clr;
     }
-    //Starts the car's engine, wrooooom
+    /**
+     * Starts the car's engine, wrooooom
+     */
     public void startEngine(){
 	    currentSpeed = 0.1;
     }
-    //Stops the cars engine, moooooorw
+    /**
+     * Stops the cars engine, moooooorw
+     */
     public void stopEngine(){
 	    currentSpeed = 0;
     }
-    //Returns the car's position.
+    /**
+     * Returns the car's position.
+     * @return
+     */
     public double[] getPosition() {
     	double[] res = {x, y};
     	return res;
     }
-    //Abstract method, used to set a speedfactor in each individual car.
+    /**
+     * Abstract method, used to set a speedfactor in each individual car.
+     * @return
+     */
     public abstract double speedFactor();
-    //Increases the speed of the car
+    /**
+     * Increases the speed of the car, takes the parameter amount and increases the speed by that amount
+     * @param amount
+     */
     public abstract void incrementSpeed(double amount);
-    //Decreases the speed of the car
+    /**
+     * Decreases the speed of the car, takes the parameter amount and decreases the speed by that amount
+     * @param amount
+     */
     public abstract void decrementSpeed(double amount);
-    //Moves the car in a given direction, it changes the x and y value depending on its direction
+    /**
+     * Moves the car in a given direction, it changes the x and y value depending on its direction
+     */
 	public void move() {
 		if (direction == NORTH)
 			y -= currentSpeed;
@@ -85,7 +126,9 @@ public abstract class Car implements Movable{
 		if (direction == EAST)
 			x += currentSpeed;
 	}
-	//Turns the car 90 degrees left
+	/**
+	 * Turns the car 90 degrees left
+	 */
 	public void turnLeft() {
 		if (direction == NORTH)
 			direction = WEST;
@@ -96,7 +139,9 @@ public abstract class Car implements Movable{
 		if (direction == EAST)
 			direction = NORTH;
 	}
-	//Turns the car 90 degrees right
+	/**
+	 * Turns the car 90 degrees right
+	 */
 	public void turnRight() {
 		if (direction == NORTH)
 			direction = EAST;
