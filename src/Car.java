d06eaa043fa87b262384cd04f551db7da6095e49
caeriks0412 <caeriks@student.chalmers.se>
2021-11-09 14:30:34 +0100

    added class car

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+import java.awt.Color;
+
+public class Car {
+
+    public int nrDoors; // Number of doors on the car
+    public double enginePower; // Engine power of the car
+    public double currentSpeed; // The current speed of the car
+    public Color color; // Color of the car
+    public String modelName; // The car model name
+	
+}
