8e76da3af7d2077867ed3933026d7d5bc6876bf0
joelgrasjostudent <grasjo@student.chalmers.se>
2021-11-09 15:33:32 +0100

    movable tillagd

--- src/Car.java
+++ src/Car.java
@@ -1,21 +1,32 @@
 import java.awt.*;
 
-public abstract class Car {
+public abstract class Car implements Movable{
 
 	private int nrDoors; // Number of doors on the car
     private double enginePower; // Engine power of the car
     private double currentSpeed; // The current speed of the car
     private Color color; // Color of the car
     private String modelName; // The car model name
+    private double x;
+    private double y;
+    private int direction;
+    
+    public static final int NORTH = 0;
+    public static final int EAST = 1;
+    public static final int SOUTH = 2;
+    public static final int WEST = 3;
+    
+    
     
     public Car(int nrDoors, Color color, int power, String model) {
-    	
     	this.nrDoors = nrDoors;
     	this.color = color;
     	this.enginePower = power;
     	this.modelName = model;
     	stopEngine();
-    	
+    	x = 0;
+    	y = 0;
+    	direction = NORTH;
     }
 	
     public int getNrDoors(){
@@ -49,11 +60,47 @@ public abstract class Car {
 	    currentSpeed = 0;
     }
     
+    public double[] getPosition() {
+    	double[] res = {x, y};
+    	return res;
+    }
+    
     public abstract double speedFactor();
 
     public abstract void incrementSpeed(double amount);
 
     public abstract void decrementSpeed(double amount);
     
-    
+	public void move() {
+		if (direction == NORTH)
+			y -= currentSpeed;
+		if (direction == SOUTH)
+			y += currentSpeed;
+		if (direction == WEST)
+			x -= currentSpeed;
+		if (direction == EAST)
+			x += currentSpeed;
+	}
+
+	public void turnLeft() {
+		if (direction == NORTH)
+			direction = WEST;
+		if (direction == SOUTH)
+			direction = EAST;
+		if (direction == WEST)
+			direction = SOUTH;
+		if (direction == EAST)
+			direction = NORTH;
+	}
+
+	public void turnRight() {
+		if (direction == NORTH)
+			direction = EAST;
+		if (direction == SOUTH)
+			direction = WEST;
+		if (direction == WEST)
+			direction = NORTH;
+		if (direction == EAST)
+			direction = SOUTH;
+	}
 }
